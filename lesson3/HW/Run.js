f44cd3aa2f665ec079d4cf2f82568b3116e70832
Justinas Kilas <JustasKilas@gmail.com>
2019-02-19 23:13:59 +0200

    HW done stage1

--- lesson3/HW/Run.js
+++ lesson3/HW/Run.js
@@ -1,73 +1,36 @@
 //constants
-
 let ctx;
 let canvas;
-let spin = true;
-let offset = 0;
-let speed = 1;
 let level = 0;
 const size = 500;
-
+const halfSize = size/2;
+const angle = degToRad(-90);
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    canvas.addEventListener('click', () => {
-        level++;
-        clearCanvas();
-        render();
-    }, false);
+    window.onkeydown = (e) => {
+        switch (e.keyCode) {
+            case 107:
+                if (level < 8) {
+                    level++;
+                    clearCanvas();
+                    render();
+                }
+                break;
+            case 109:
+                if (level > 0) {
+                    level--;
+                    clearCanvas();
+                    render();
+                }
+                break;
+        }
+        if (e.key > 0 && e.key < 10) {
+            numberToRotate = Number(e.key);
+        }
+    };
     render();
-}
-
-let angle = 90;
-
-function drawFractal(fractalStages) {
-    if (fractalStages === 0) {
-        drawShape();
-    } else {
-        let stage = 0;
-        let toMinus = 1;
-        if (fractalStages < 1) {
-            toMinus = 0;
-        }
-        ctx.save();
-        if (fractalStages === stage) {
-            ctx.fillStyle = '#FF0000';
-            ctx.strokeStyle = '#FF0000';
-        }
-        transformF1(toMinus);
-        drawFractal(fractalStages - toMinus);
-        ctx.restore();
-
-        ctx.save();
-        if (fractalStages === stage) {
-            ctx.fillStyle = '#00FF00';
-            ctx.strokeStyle = '#00FF00';
-        }
-        transformF2(toMinus);
-        drawFractal(fractalStages - toMinus);
-        ctx.restore();
-
-        ctx.save();
-        if (fractalStages === stage) {
-            ctx.fillStyle = '#0000FF';
-            ctx.strokeStyle = '#0000FF';
-        }
-        transformF3(toMinus);
-        drawFractal(fractalStages - toMinus);
-        ctx.restore();
-
-        ctx.save();
-        if (fractalStages === stage) {
-            ctx.fillStyle = '#00FFFF';
-            ctx.strokeStyle = '#00FFFF';
-        }
-        transformF4(toMinus);
-        drawFractal(fractalStages - toMinus);
-        ctx.restore();
-
-    }
 }
 
 function drawShape(step) {
@@ -78,47 +41,43 @@ function drawShape(step) {
         ctx.save();
         ctx.save();
         if (step + 1 === level) {
-           // ctx.fillStyle = '#dddd55';
+            ctx.fillStyle = '#dddd55';
         }
         ctx.transform(0.5, 0, 0, 0.5, 0, 0);
         drawShape(step);
         ctx.restore();
         if (step + 1 === level) {
-           // ctx.fillStyle = '#55dd55';
+            ctx.fillStyle = '#55dd55';
         }
-        ctx.transform(0.5, 0, 0, -0.5, size / 2, size / 2);
+        ctx.transform(0.5, 0, 0, -0.5, halfSize, halfSize);
         drawShape(step);
         ctx.restore();
         if (step + 1 === level) {
-           // ctx.fillStyle = '#dd5555';
+            ctx.fillStyle = '#dd5555';
         }
-        ctx.transform(0.5, 0, 0, -0.5, size / 2, size / 2);
-        ctx.rotate(degToRad(-90));
+        ctx.transform(0.5, 0, 0, -0.5, halfSize, halfSize);
+        ctx.rotate(angle);
         drawShape(step);
         ctx.restore();
-        ctx.transform(0.25, 0, 0, 0.25, size / 4, size / 4 * 3);
-        ctx.rotate(degToRad(-90));
+        ctx.transform(0.25, 0, 0, 0.25, halfSize/2, halfSize/2 * 3);
+        ctx.rotate(angle);
         drawShape(step);
     } else drawT();
 }
 
 function drawT() {
-    // Filled red triangle
-    //ctx.fillStyle = "rgb(0,0,0)";
-    ctx.translate(size / 2, size / 2);
+    ctx.translate(halfSize, halfSize);
     ctx.beginPath();
     ctx.moveTo(0, 0);
-    ctx.lineTo(size / 2, -size / 2);
-    ctx.lineTo(size / 2, size / 2);
-    ctx.lineTo(-size / 2, size / 2);
+    ctx.lineTo(halfSize, -halfSize);
+    ctx.lineTo(halfSize, halfSize);
+    ctx.lineTo(-halfSize, halfSize);
 
-    ctx.lineTo(-size / 2, -size / 3);
-    ctx.lineTo(size / 3, -size / 3  );
-    ctx.lineTo(size / 3, -size / 2);
+    ctx.lineTo(-halfSize, -size / 3);
+    ctx.lineTo(size / 3, -size / 3);
+    ctx.lineTo(size / 3, -halfSize);
 
-    ctx.lineTo(size / 2, -size / 2);
-
-    //ctx.closePath();
+    ctx.lineTo(halfSize, -halfSize);
     ctx.fill();
 }
 
@@ -126,7 +85,7 @@ function render() {
     ctx.save();
     console.log(level);
     ctx.translate(1, 1);
-    ctx.scale(1.5,1.5);
+    ctx.scale(1.5, 1.5);
     drawShape(level);
     ctx.restore();
 }
